3bc74caa521c18611188fa890f6a32b1843a40e2
rubcuadra <rubcuadra@gmail.com>
2017-08-24 13:57:25 -0500

    Ready to go

--- Proyecto 2/Proyecto3.py
+++ Proyecto 2/Proyecto3.py
@@ -24,6 +24,7 @@ def normalizarMedia(vector): #Vector con valores de X
 #   Un vector mu que contiene las medias de cada columna
 #   Un vector sigma que contiene las deviaciones estandares por columna
 def normalizacionDeCaracteristicas(X): #VALIDAR SI mu y sigma SON DEL _X o X
+    if type(X).__module__ != np.__name__: X = np.array(X)
     _X = np.apply_along_axis( normalizarMedia , 0, X) #Normalizar por cada columna de X
     mu = np.mean(X,axis=0)                            #Vector con medias
     sigma = np.std(X,axis=0)                          #Deviaciones estandares
@@ -36,12 +37,11 @@ def ecuacionNormal(X,Y):
     fixedX = np.append(np.ones((X.shape[0],1)), X , axis=1) #Agregar ones a X
     return  (inv(fixedX.transpose().dot(fixedX))).dot(fixedX.transpose().dot(Y))
 
-def calculaCosto(X,Y,theta):
-    val = 0
-    for _x,_y in zip(X,Y):
-        val += np.power( (hipothesis(_x,theta) - _y) , 2) 
-    val/(2*len(Y))
-    return val
+def calculaCosto(x,y,theta):
+    r = 0
+    for (_x,_y) in zip(x,y):
+        r += (hipothesis(_x,theta)-_y)**2
+    return r/(2*len(x))
 
 def graficaError(historial):
     plt.plot( historial, label='Historial del error')
@@ -51,7 +51,7 @@ def graficaError(historial):
 def hipothesis(vX,thetas):
     return thetas.transpose().dot(vX)
 
-def gradienteDescendenteMultivariable(X,Y,theta=None,alpha=0.1,iteraciones=100):
+def gradienteDescendenteMultivariable(X,Y,theta=None,alpha=0.01,iteraciones=1500):
     '''Validar que todo sea Numpy array/matrix'''
     if type(X).__module__ != np.__name__: X = np.array(X)
     if type(Y).__module__ != np.__name__: Y = np.array(Y)
@@ -62,16 +62,16 @@ def gradienteDescendenteMultivariable(X,Y,theta=None,alpha=0.1,iteraciones=100):
     jHistoria = np.zeros(iteraciones)                       #Historial de iteraciones
     fixedX = np.append(np.ones((X.shape[0],1)), X , axis=1) #Agregar ones a X
     
-    #hipothesis(fixedX[0],theta)
     for it in range(0,iteraciones): 
-        tempTheta = theta.copy() #Thetas temporales
-        for j in range(0, len(theta) ):
+        tempThetas = theta.copy() #Thetas temporales
+        for j in range(0, len(tempThetas) ):
             #FORMULA thetaI = thetaI-(alfa/m)*sum( (hip(xi)-yi)*x[j]i  )
-            tempTheta[j] = theta[j]-(alpha/len(X))*sum((hipothesis(valX,theta)-valY)*valX[j] for valX,valY in zip(fixedX,Y))
-        theta = tempTheta   #Preparar para proxima iteracion
-        jHistoria[it]  = calculaCosto(fixedX,Y,theta)   #Guardar costo
+            tempThetas[j] = theta[j] - (alpha/len(X))*sum( ( hipothesis(_x,theta)-_y)*_x[j] for (_x,_y) in zip(fixedX,Y) )
+        theta = tempThetas
+        jHistoria[it]  = calculaCosto(fixedX,Y,theta)   #Guardar costos
     return (jHistoria,theta)
 
 def predicePrecio(X,theta):
     if type(X).__module__ != np.__name__: X = np.array(X)
+    if type(theta).__module__ != np.__name__: theta = np.array(theta)
     return hipothesis( np.append([1],X) ,theta)
--- Proyecto 2/main.py
+++ Proyecto 2/main.py
@@ -1,16 +1,25 @@
 from Proyecto3 import *
 
+def buscarAlfa(xNormalizadas,Y,alpha=0.001,iteraciones=50, multiplicador=3):
+    r, alph = float("inf"), alpha
+    while alpha<2:
+        jHistoriaGradienteNormalizado, thetasGradienteNormalizado  = gradienteDescendenteMultivariable(nX,yData, alpha=alpha ,iteraciones=50)
+        if jHistoriaGradienteNormalizado[-1] < r:
+            alph = alpha
+            r = jHistoriaGradienteNormalizado[-1]
+            #graficaError(jHistoriaGradienteNormalizado)    
+        alpha *= multiplicador
+    return alph, r #Regresa alpha y el ultimo costo 
+
 if __name__ == "__main__":
+    #Leer archivos
     xData, yData = getDataFromFile('datos.csv')
+    #Normalizar
     nX, mediasX, sigma = normalizacionDeCaracteristicas(xData)
-    
-    jHistoriaGradiente, thetasGradiente  = gradienteDescendenteMultivariable(nX,yData,alpha=0.2) 
+    jHistoriaGradienteNormalizado, thetasGradienteNormalizado  = gradienteDescendenteMultivariable(nX,yData, alpha=0.729 ,iteraciones=1500)
     thetasEcuacion = ecuacionNormal(xData,yData)
     
-    print thetasGradiente
-    print thetasEcuacion
-
-    # print "{:,}".format( predicePrecio(xData[0],thetasNorm) ) 
-    # print "{:,}".format( predicePrecio(xData[0],thetas2) ) 
-
-    graficaError(jHistoriaGradiente)
+    print ('Thetas usando gradiente normalizado ', thetasGradienteNormalizado)
+    print ('Thetas de la ecuacion normal ', thetasEcuacion)
+    print ("Buena alpha: ",buscarAlfa(nX,yData)[0]) #Nos da el 0.729 buscado
+    graficaError(jHistoriaGradienteNormalizado)    
