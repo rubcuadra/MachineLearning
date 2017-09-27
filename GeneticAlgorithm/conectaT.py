9a54b4cd57a2373534e976970b90899f967bb208
rubcuadra <rubcuadra@gmail.com>
2017-09-27 16:04:08 -0500

    added ex obtain item

--- GeneticAlgorithm/conectaT.py
+++ GeneticAlgorithm/conectaT.py
@@ -6,6 +6,9 @@ class Board():
 	def __init__(self, nativeBoard):
 	    self.val = np.array(nativeBoard).transpose()[::-1,:] #Fix para que salga como deberia
 
+	def __getitem__(self,i):
+         return self.val[i]
+
 	def getCombinations(self, diskToThrow):
 		cols = self.val.shape[0] # 0 -> Cols ; 1 -> Rows
 		combinations = []
@@ -19,14 +22,17 @@ class Board():
 
 	def getBoardScore(boardToTest, ourDisk):
 		#Heuristic
+		print (boardToTest)
+		print (boardToTest[-1][1]) #Fila de hasta abajo, columna 1
 		return randint(100,8000) #Un numero grande
+
 	def __str__(self):
 		return str(self.val)
 
 def getBestColToPlay(nativeBoard,disk,totalColumns=7):
+	ff = np.vectorize( Board.getBoardScore ) #Evaluar cada tablero y regresar su Score								 
 	_board = Board(nativeBoard) #Crear un tablero nuestro
-	print(_board)
-	ff = np.vectorize( Board.getBoardScore ) #Evaluar cada tablero y regresar su Score        										 
+
 	population       = _board.getCombinations(disk)
 	#Por cada tablero en population podemos obtener nuevas populations -> Bajar en el arbol
 	populationScores = ff( population,disk ) 
