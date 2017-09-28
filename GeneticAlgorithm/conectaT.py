a73b870fee4565cb9470899fd470cd7d2395b32c
rubcuadra <rubcuadra@gmail.com>
2017-09-28 17:13:52 -0500

    colScore 1 lvl

--- GeneticAlgorithm/conectaT.py
+++ GeneticAlgorithm/conectaT.py
@@ -3,9 +3,30 @@ from copy import copy
 from random import random,randint
 from sys import maxsize
 
+class BoardNode():
+    def __init__(self, data):
+        self.data = data
+        self.children = []
+
+    def add_children_boards(self, boardsArray):
+        for board in boardsArray:
+            self.children.append( BoardNode(board) )
+
+    def getMax(self): #Nos devuelve el hijo con mejor score y su index
+        maxi = 0
+        maxScore = Board.minScore
+        for i,c in enumerate(self.children):     #Nodos Hijos
+            if c.data is None: continue
+            if maxScore<c.data.score:
+                maxi = i
+                maxScore = c.data.score
+        return (maxi,maxScore)
+
 class Board():
+    minScore = -1*maxsize
     def __init__(self,nativeBoard=None):
         self.val = None
+        self.score = self.minScore
         if nativeBoard:
             self.val = np.array(nativeBoard).transpose()[::-1,:] #Fix para que salga como deberia
 
@@ -15,19 +36,29 @@ class Board():
     def __str__(self):
         return str(self.val)
 
+    def __gt__(self, other):
+        return self.score > other.score
+
+    def __ge__(self, other):
+        return self.score >= other.score
+
     def __copy__(self):
         newone = type(self)()
         newone.val = self.val.copy()
         return newone
 
     def getCombinations(self, diskToThrow): #Regresamos copias
-        return [Board.throw(copy(self),i,diskToThrow) for i in range(self.val.shape[1])]
+        gc = []
+        for i in range(self.val.shape[1]):            
+            gc.append(Board.throw(copy(self),i,diskToThrow))
+        return gc 
     
     def throw(brd,col_i,disk):
         ix = np.where(brd.val[:,col_i]==0)[0]
-        if len(ix)==0:return None #Ya no se puede tirar ahi
-        brd.val[:,col_i][ix[-1]] = disk
-        return brd                #Regresar tablero modificado
+        if len(ix)==0:return None                    #Ya no se puede tirar ahi
+        brd.val[:,col_i][ix[-1]] = disk              #Hacer el tiro
+        brd.score = Board.getBoardScore(brd,disk,col_i) #Actualizar el score
+        return brd                                   #Regresar tablero modificado
 
     def getSize(self):
         return self.val.shape
@@ -38,13 +69,11 @@ class Board():
         except: return None
 
     def getBoardScore(_board, ourDisk, movedColumn):
-        if not _board:        #No hay board
-            return -1*maxsize      #Regresar - demasiados puntos
-        
+        if not _board: return Board.minScore  #Regresar el minimo   
         rows,columns = _board.getSize() #Saber si ya nos pasamos de rows o columns
         #movedColumn = movedColumn      #Es el que nos pasaron
         movedRow     = _board.val[:,movedColumn].nonzero()[0][0]
-
+        
         #Celdas para validar, pueden ser None
         current = _board[movedRow][movedColumn]
         der = _board.g(movedRow,movedColumn+1)
@@ -59,17 +88,13 @@ class Board():
         
         return randint(100,8000) #Un numero grande
 
-def getScores(boards,disk):
-    r = np.array([])
-    for i,b in enumerate(boards):
-        r = np.append(r, Board.getBoardScore(b,disk,i))
-    return r
-
-def getBestColToPlay(nativeBoard,disk,totalColumns=7):
+def getBestColToPlay(nativeBoard,disk,totalColumns=7,depth=3):
     _board = Board(nativeBoard) #Crear un tablero nuestro
-    population       = _board.getCombinations(disk)
-    #Por cada tablero en population podemos obtener nuevas populations -> Bajar en el arbol
-    populationScores = getScores(population,disk)
-    #Si quisieramos a modo de arbol seria loopear para sacar el bestSpecimen
-    bestSpecimen     = np.argmax(populationScores)
-    return bestSpecimen
+    
+    root = BoardNode( _board )
+    root.add_children_boards( _board.getCombinations(disk) )
+    # for i in range(depth-1):                        #depths hacia abajo
+    #     d = disk if not i%2 else 666 #cualquier otro numero
+    #     calculateScores(population,d)    
+    colToThrow,colScore = root.getMax()
+    return colToThrow
